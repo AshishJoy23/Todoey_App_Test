89531657f89da0efb54b771a059619d3353e2f4f
AshishJoy23 <ashishjoy23@gmail.com>
2024-03-07 18:00:52 +0530

    final commit

--- lib/view/task/task_screen.dart
+++ lib/view/task/task_screen.dart
@@ -102,140 +102,142 @@ class _TaskScreenState extends State<TaskScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            TextField(
-              controller: _titleController,
-              decoration: const InputDecoration(labelText: 'Title'),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _contentController,
-              maxLines: 3,
-              decoration: const InputDecoration(labelText: 'Content'),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _dateController,
-              readOnly: true,
-              onTap: () => _selectDate(context),
-              decoration: InputDecoration(
-                labelText: 'Select Date',
-                suffixIcon: Icon(
-                  Icons.calendar_today,
-                  color: Theme.of(context).colorScheme.primary,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextField(
+                controller: _titleController,
+                decoration: const InputDecoration(labelText: 'Title'),
+              ),
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: _contentController,
+                maxLines: 3,
+                decoration: const InputDecoration(labelText: 'Content'),
+              ),
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: _dateController,
+                readOnly: true,
+                onTap: () => _selectDate(context),
+                decoration: InputDecoration(
+                  labelText: 'Select Date',
+                  suffixIcon: Icon(
+                    Icons.calendar_today,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _timeController,
-              readOnly: true,
-              onTap: () => _selectTime(context),
-              decoration: InputDecoration(
-                labelText: 'Select Time',
-                suffixIcon: Icon(
-                  Icons.access_time,
-                  color: Theme.of(context).colorScheme.primary,
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: _timeController,
+                readOnly: true,
+                onTap: () => _selectTime(context),
+                decoration: InputDecoration(
+                  labelText: 'Select Time',
+                  suffixIcon: Icon(
+                    Icons.access_time,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20),
-              child: Row(
-                mainAxisAlignment: widget.isAdd
-                    ? MainAxisAlignment.center
-                    : MainAxisAlignment.spaceEvenly,
-                children: [
-                  widget.isAdd
-                      ? Container()
-
-                      /// Delete Task Button
-                      : Container(
-                          width: 150,
-                          height: 55,
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  width: 2),
-                              borderRadius: BorderRadius.circular(15)),
-                          child: MaterialButton(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            minWidth: 150,
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Row(
+                  mainAxisAlignment: widget.isAdd
+                      ? MainAxisAlignment.center
+                      : MainAxisAlignment.spaceEvenly,
+                  children: [
+                    widget.isAdd
+                        ? Container()
+          
+                        /// Delete Task Button
+                        : Container(
+                            width: 150,
                             height: 55,
-                            onPressed: () async {
-                              final result = await confirmDialog(context);
-                              if (result != null && result) {
-                                await _dbServices.deleteTask(
-                                    task: widget.task!);
-                                showSnackbarMsg(
-                                    context, 'Task deleted successfully');
-                                Navigator.pop(context);
-                              }
-                            },
-                            color: Colors.white,
-                            child: Text(
-                              'Delete Task',
-                              style: TextStyle(
-                                  color: Theme.of(context).colorScheme.primary,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Theme.of(context).colorScheme.primary,
+                                    width: 2),
+                                borderRadius: BorderRadius.circular(15)),
+                            child: MaterialButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              minWidth: 150,
+                              height: 55,
+                              onPressed: () async {
+                                final result = await confirmDialog(context);
+                                if (result != null && result) {
+                                  await _dbServices.deleteTask(
+                                      task: widget.task!);
+                                  showSnackbarMsg(
+                                      context, 'Task deleted successfully');
+                                  Navigator.pop(context);
+                                }
+                              },
+                              color: Colors.white,
+                              child: Text(
+                                'Delete Task',
+                                style: TextStyle(
+                                    color: Theme.of(context).colorScheme.primary,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16),
+                              ),
                             ),
                           ),
-                        ),
-
-                  /// Add or Update Task Button
-                  MaterialButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    minWidth: 150,
-                    height: 55,
-                    onPressed: () async {
-                      if (_titleController.text == '') {
-                        showSnackbarMsg(context, 'Title is required one');
-                      } else {
-                        if (widget.isAdd) {
-                          TaskModel task = TaskModel.create(
-                            title: _titleController.text,
-                            subtitle: _contentController.text,
-                            taskDate: _dateController.text,
-                            taskTime: _timeController.text,
-                          );
-                          await _dbServices.addTask(task: task);
-                          showSnackbarMsg(
-                              context, 'New task added successfully');
+          
+                    /// Add or Update Task Button
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      minWidth: 150,
+                      height: 55,
+                      onPressed: () async {
+                        if (_titleController.text == '') {
+                          showSnackbarMsg(context, 'Title is required one');
                         } else {
-                          widget.task!.title = _titleController.text;
-                          widget.task!.subtitle = _contentController.text;
-                          widget.task!.taskDate = _dateController.text;
-                          widget.task!.taskTime = _timeController.text;
-                          await _dbServices.updateTask(task: widget.task!);
-                          showSnackbarMsg(context, 'Task updated successfully');
+                          if (widget.isAdd) {
+                            TaskModel task = TaskModel.create(
+                              title: _titleController.text,
+                              subtitle: _contentController.text,
+                              taskDate: _dateController.text,
+                              taskTime: _timeController.text,
+                            );
+                            await _dbServices.addTask(task: task);
+                            showSnackbarMsg(
+                                context, 'New task added successfully');
+                          } else {
+                            widget.task!.title = _titleController.text;
+                            widget.task!.subtitle = _contentController.text;
+                            widget.task!.taskDate = _dateController.text;
+                            widget.task!.taskTime = _timeController.text;
+                            await _dbServices.updateTask(task: widget.task!);
+                            showSnackbarMsg(context, 'Task updated successfully');
+                          }
+          
+                          Navigator.pop(context);
                         }
-
-                        Navigator.pop(context);
-                      }
-                    },
-                    color: Theme.of(context).colorScheme.primary,
-                    child: Text(
-                      widget.isAdd ? 'Add Task' : 'Update Task',
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 16),
+                      },
+                      color: Theme.of(context).colorScheme.primary,
+                      child: Text(
+                        widget.isAdd ? 'Add Task' : 'Update Task',
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 16),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
